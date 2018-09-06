7b6e9027f9650d6d6bd238f9128a62bec552b358
sjg99 <38005341+sjg99@users.noreply.github.com>
2018-09-06 12:13:07 -0500

    Fixing Design

--- Database.php
+++ Database.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Do+Hyeon|Fjalla+One|Metal+Mania" rel="stylesheet">
     <title>Database Manager</title>
 </head>
 
 <body>
+    <Section class="TSel">
+        <h1>SQL Select</h1>
     <?php
     include 'SqlConn.php';
     $sql = "SELECT * FROM User";
@@ -24,41 +28,50 @@
     }
     $conn->close();
     ?>
+    </section>
+    <section class="FIns">
+    <h1>SQL Insert</h1>
     <form action="SqlInsert.php" method="post">
-    <p>
-        <label>Username:</label>
-        <input type="text" name="Username">
+    <p>       
+        <input type="text" name="Username" placeholder="Username">
     </p>
     <p>
-        <label>Email:</label>
-        <input type="email" name="Email">
+       
+        <input type="email" name="Email" placeholder="Email">
     </p>
     <p>
-        <label>Password:</label>
-        <input type="password" name="Password">
+        
+        <input type="password" name="Password" placeholder="Password">
     </p>
     <p>
-        <label>Birth Date:</label>
-        <input type="date" name="BirthDate">
+        
+        <input type="date" name="BirthDate" placeholder="BirthDate">
     </p>
     <p>
-        <label>Position:</label>
-        <input type="text" name="Position">
+        
+        <input type="text" name="Position" placeholder="Position">
     </p>    
     <p>
-        <label>Document ID:</label>
-        <input type="text" name="DocumentID">
+        
+        <input type="text" name="DocumentID" placeholder="DocumentID">
     </p>
     <p>
-        <label>Cellphone:</label>
-        <input type="number" name="Cellphone">
+        
+        <input type="number" name="Cellphone" placeholder="Cellphone">
     </p>
     <p>
-        <label>Address:</label>
-        <input type="text" name="Address">
+        
+        <input type="text" name="Address" placeholder="Address">
     </p>
-    <button type="submit">Insert</button>
+    <button type="submit">Insert</button>    
 </form>
+</section>
+<section class="Links">
+        <a class="ND" href="index.html">Go Back</a>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
 </html>
